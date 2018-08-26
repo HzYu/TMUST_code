73aeacaa172481e34cd7faafe3c24e969a3afead
RatiSmiDB <tim406she1016@gmail.com>
2018-08-26 16:16:08 +0800

    rm

--- Web_ex/PHP/member_system/delect.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once("connectdb.php");
-$sql_delect="DELETE FROM `student` WHERE id= $_GET[id]";  //sql刪除語法
-mysql_query($sql_delect);
-//echo $sql_delect;
-header("location:score.php");
-?>
