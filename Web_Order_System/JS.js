a1af9d7ab679ed36d152a02413c765e2d9096968
RatiSmiDB <tim406she1016@gmail.com>
2018-09-01 23:12:40 +0800

    JS

---
+++ Web_Order_System/JS.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    $("#Join").click(function(){
+        $("html, body").animate({
+            scrollTop : $("#member_Join").offset().top 
+        },1000);
+    });
+});
+
+$(document).ready(function(){
+    $("#Sign_up").click(function(){
+        $("html, body").animate({
+            scrollTop : $("#member_Sign_up").offset().top 
+        },1000);
+    });
+});
+
+$(document).ready(function(){
+    $("#Menu_link").click(function(){
+        $("html, body").animate({
+            scrollTop : $("#Menu_link1").offset().top 
+        },1000);
+    });
+});
+
+$(document).ready(function(){
+    $("#Message_Link").click(function(){
+        $("html, body").animate({
+            scrollTop : $("#Message_Link1").offset().top 
+        },1000);
+    });
+});
+
+$(document).ready(function(){
+    $("#Join_Link").click(function(){
+        $("html, body").animate({
+            scrollTop : $("#Join_Link1").offset().top 
+        },1000);
+    });
+});
